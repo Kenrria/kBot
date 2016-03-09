3b35c11c55fd3bbb0c96c6eced01248054df805d
Kenrria <cruzoliverajesus@gmail.com>
2016-03-09 19:36:10 +0100

    Update ActualizadorEstado.java

--- BotTwitter/src/ActualizadorEstado.java
+++ BotTwitter/src/ActualizadorEstado.java
@@ -15,32 +15,24 @@ public class ActualizadorEstado {
 	void setup()
 	{
 	    ConfigurationBuilder cb = new ConfigurationBuilder();
-	    cb.setOAuthConsumerKey("Kq3yXqsZ6wUpaYvVi0qExiggb");
-	    cb.setOAuthConsumerSecret("Ia9QPBUWvHVIx6MCeaZO6Lq7HP25nYHJlNuOKSEbrbhe9BrTLK");
-	    cb.setOAuthAccessToken("705403074479833088-bjcIsU3lyoReodWYVCqKIu1ilRhNa5C");
-	    cb.setOAuthAccessTokenSecret("fpFatozSUJGuEzSz3kWxMeZ2qTbyJ6Sse5sj0a9Aiauug");
+	    cb.setOAuthConsumerKey(AuthInfo.OAuthConsumerKey);
+	    cb.setOAuthConsumerSecret(AuthInfo.OAuthConsumerKey);
+	    cb.setOAuthAccessToken(AuthInfo.OAuthAccessToken);
+	    cb.setOAuthAccessTokenSecret(AuthInfo.OAuthAccessTokenSecret);
 	    TwitterFactory tf = new TwitterFactory(cb.build());
 	    twitter = tf.getInstance();
 	}
 	
 	void twitear(){
-		buscarPalabras();
-		
-	}
-	
-
-
-	
-
-	private void buscarPalabras() {
 		try {
-			Process p =Runtime.getRuntime().exec("/home/jesus/buscarPalabras.sh ");
-			
-			
-		} catch (IOException e) {
-		} 
-		
+			twitter.updateStatus("test");
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+		
+	
 
 	public static void main (String[] args){
 		ActualizadorEstado actualizador = new ActualizadorEstado();
@@ -50,72 +42,3 @@ public class ActualizadorEstado {
 
 }
 
-
-
-
-
-
-
-
-//try {
-/*Scanner sc = new Scanner(System.in);
-String mensaje=sc.next();*/
-
-//String mensaje = randomYTUrl();
-/*Status status = twitter.updateStatus(mensaje);
-System.out.println("actualizado");
-} catch (TwitterException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-System.out.println("No actualizado");
-}*/
-
-
-
-//void retwitear(){
-	/*try {
-		IDs friends = twitter.getFriendsIDs("KBot92",-1);
-		
-		for (long i : friends.getIDs())
-		   {
-				//System.out.println(twitter.showUser(i).getName());
-				//Status status = twitter.updateStatus(twitter.showUser(i).getName());
-				/*List<Status> listaStatus = null;
-				listaStatus = twitter.getUserTimeline(twitter.showUser(i).getName());
-					for (Status status : listaStatus) {
-							System.out.println(listaStatus);
-					}
-			//twitter.retweetStatus();
-			
-			twitter.getRetweeterIds()
-		   }
-	
-		System.out.println("actualizado");
-	} catch (TwitterException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-		System.out.println("No actualizado");
-	}*/
-//}
-
-/*
-private String randomYTUrl() {
-for (int i = 0; i < 11; i++ ){
-	Random r1 = new Random();
-	int mayusOminus = r1.nextInt((1-0) +1) +0;
-	
-	if ( mayusOminus == 0){
-		Random r = new Random();
-		char aux = (char)(r.nextInt(26) + 'a');
-		System.out.print(aux);
-	} 
-	if ( mayusOminus == 1){
-		Random r = new Random();
-		char aux = (char)(r.nextInt(26) + 'A');
-		System.out.print(aux);
-	}
-	
-	
-}
-return null;
-}*/
